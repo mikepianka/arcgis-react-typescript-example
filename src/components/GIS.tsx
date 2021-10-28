79ea5e0c3b8916b84fee21f4ac1ec4618947109d
Mike Pianka <47948499+mikepianka@users.noreply.github.com>
2021-10-28 15:30:13 -0400

    basic setup

---
+++ src/components/GIS.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from "react";
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+import Search from "@arcgis/core/widgets/Search";
+import "./GIS.css";
+
+interface IProps {
+  center: Array<number>;
+  zoom: number;
+}
+
+export default function GIS({ center, zoom }: IProps) {
+  const mapDiv = useRef(null);
+
+  useEffect(() => {
+    if (mapDiv.current) {
+      const map = new Map({ basemap: "topo" });
+
+      const view = new MapView({
+        map,
+        center,
+        zoom,
+        container: mapDiv.current,
+      });
+
+      const searchWidget = new Search({ view });
+      view.ui.add(searchWidget, "top-right");
+    }
+  }, [center, zoom]);
+
+  return <div id="gisDiv" ref={mapDiv}></div>;
+}
